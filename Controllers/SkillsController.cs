0c985b196466f9334dbd873e96ff75cc029b7636
Pearce Odegard <pearceodegard@gmail.com>
2020-11-03 17:39:15 -0600

    Bonus complete: Form reformatted to table, new results path added

--- Controllers/SkillsController.cs
+++ Controllers/SkillsController.cs
@@ -25,34 +25,72 @@ namespace SkillsTracker.Controllers
         [Route("form")]
         public IActionResult Form()
         {
-            string html = "<form method='post' action='/skills/form'>" +
-                "<h2>Date:<h2>" +
-                "<input type='date' name='date'/>" +
-                "<h2>Python:</h2>" +
-                "<select name='python'>" +
-                    "<option>Not Started</option>" +
-                    "<option>Learning Basics</option>" +
-                    "<option>Making Apps</option>" +
-                "</select>" +
-                "<h2>Puppeteer</h2>" +
-                "<select name='puppeteer'>" +
-                    "<option>Not Started</option>" +
-                    "<option>Learning Basics</option>" +
-                    "<option>Making Apps</option>" +
-                "</select>" +
-                "<h2>C#, ASP.NET</h2>" +
-                "<select name='aspNET'>" +
-                    "<option>Not Started</option>" +
-                    "<option>Learning Basics</option>" +
-                    "<option>Making Apps</option>" +
-                "</select>" +
-                "<button type='submit'>Submit</button>" +
+            string html = "<form method='post' action='/skills/results'>" +
+                "<table border='1'>" +
+                "<tr>" +
+                "<td>Date</td>" +
+                "<td><input type='date' name='date'/></td>" +
+                "</tr>" +
+                "<tr>" +
+                    "<td>Python level</td>" +
+                    "<td>" +
+                        "<select name='python'>" +
+                            "<option>Not Started</option>" +
+                            "<option>Learning Basics</option>" +
+                            "<option>Making Apps</option>" +
+                        "</select>" +
+                    "</td>" +
+                "</tr>" +
+                "<tr>" +
+                    "<td>Puppeteer level</td>" +
+                    "<td>" +
+                        "<select name='puppeteer'>" +
+                            "<option>Not Started</option>" +
+                            "<option>Learning Basics</option>" +
+                            "<option>Making Apps</option>" +
+                        "</select>" +
+                    "</td>" +
+                "</tr>" +
+                "<tr>" +
+                    "<td>C#, ASP.NET level</td>" +
+                    "<td>" +
+                        "<select name='aspNET'>" +
+                            "<option>Not Started</option>" +
+                            "<option>Learning Basics</option>" +
+                            "<option>Making Apps</option>" +
+                        "</select>" +
+                    "</td>" +
+                "</tr>" +
+                "</table>" +
+                "<p><input type='submit' value='submit'/></p>" +
                 "</form>";
+                //"<h2>Date:<h2>" +
+                //"<input type='date' name='date'/>" +
+                //"<h2>Python:</h2>" +
+                //"<select name='python'>" +
+                //    "<option>Not Started</option>" +
+                //    "<option>Learning Basics</option>" +
+                //    "<option>Making Apps</option>" +
+                //"</select>" +
+                //"<h2>Puppeteer</h2>" +
+                //"<select name='puppeteer'>" +
+                //    "<option>Not Started</option>" +
+                //    "<option>Learning Basics</option>" +
+                //    "<option>Making Apps</option>" +
+                //"</select>" +
+                //"<h2>C#, ASP.NET</h2>" +
+                //"<select name='aspNET'>" +
+                //    "<option>Not Started</option>" +
+                //    "<option>Learning Basics</option>" +
+                //    "<option>Making Apps</option>" +
+                //"</select>" +
+                //"<button type='submit'>Submit</button>" +
+                //"</form>";
 
             return Content(html, "text/html");
         }
 
-        [HttpPost("form")]
+        [HttpPost("results")]
         public IActionResult FormSubmit(string date, string python, string puppeteer, string aspNET)
         {
             string html = $"<h1>{date}</h1>" +
